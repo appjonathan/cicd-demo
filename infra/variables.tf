657354bba69add0bc0856eedaa865ccf2d3485dc
eppjonathan <epp.j.epp@googlemail.com>
2024-01-31 15:50:33 +0100

    first commit

---
+++ infra/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  type    = string
+  default = "eu-central-1"
+}
+
+variable "aws_profile" {
+  type    = string
+  default = "Student-319551831060"
+}
---
+++ my-react-app/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+    <h1>My Techstarter Website, Jonathan...</h1>
+  );
+}
+
+export default App;
